09819c84307b0bf4cdd1bdb22e10d663fafa21c4
Zain ul abdien <74106328+Surge69420@users.noreply.github.com>
2025-03-21 16:53:58 +0000

    Enhance user interface and add user image support
    - Added a new search form and profile navigation button in `Index.cshtml`.
    - Updated sign-out form layout and improved image display logic.
    - Included a password recovery link in the password change section.
    - Introduced `Users` class in `Users.cs` to support user image storage.

---
+++ Data/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Data.Models
+{
+    public class Users : IdentityUser
+    {
+        public byte[]? ImageData { get; set; }
+    }
+}
